888cb14afbc60c26b29208fe074fe0077acafa0e
Ariane <ariianeboorges@gmail.com>
2021-05-05 19:03:29 +0100

    Add form to modal and create style to the form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Modal from 'react-modal';
+import { Container } from './styles';
+
+import closeImg from '../../assets/close.svg';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -11,8 +14,28 @@ export function NewTransactionModal({
   onRequestClose,
 }: NewTransactionModalProps): JSX.Element {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-      <h2>Register Transaction</h2>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Close modal" />
+      </button>
+
+      <Container>
+        <h2>Register Transaction</h2>
+
+        <input placeholder="Title" />
+        <input placeholder="Value" type="number" />
+        <input placeholder="Category" />
+        <button type="submit">Register</button>
+      </Container>
     </Modal>
   );
 }
