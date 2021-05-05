5bdfbc8dd70993f93c36aa820e15863d1ad90a5d
Ariane <ariianeboorges@gmail.com>
2021-05-05 11:09:05 +0100

    Create and style Header including new transaction button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from '../../assets/logo.svg';
+import { Container, Content } from './style';
 
 export function Header(): JSX.Element {
   return (
-    <header>
-      <img src={logo} alt="dt money" />
-      <button type="button">New transaction</button>
-    </header>
+    <Container>
+      <Content>
+        <img src={logo} alt="dt money" />
+        <button type="button">New transaction</button>
+      </Content>
+    </Container>
   );
 }
