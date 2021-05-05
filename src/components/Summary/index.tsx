6ee6d970983e88631e3757783e6af7362da4428f
Ariane <ariianeboorges@gmail.com>
2021-05-05 11:15:43 +0100

    Create Summary

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from './styles';
+
+export function Summary(): JSX.Element {
+  return (
+    <Container>
+      <h1>summary</h1>
+    </Container>
+  );
+}
